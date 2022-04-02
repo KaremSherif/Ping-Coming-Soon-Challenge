781b02c49bbbf1dc5f9a1ff903884e8f158f528e
Karem Sherif <karemsherif922@gmail.com>
2022-04-02 14:45:18 +0200

    Challenge Done

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+const btn = form.querySelector('[type="submit"]');
+const errorMsg ='Please provide a vaild email address';
+
+const error = document.createElement('p');
+error.classList.add('error-text')
+error.textContent = errorMsg;
+
+function checkEmail(e){
+    e.preventDefault();
+    const email = e.target.querySelector('[type="email"]').value
+    if(!isEmail(email)){
+        form.classList.add('error')
+        form.insertBefore(error, btn)
+        // console.log('invaild')
+    }else{
+        form.classList.remove('error')
+        form.removeChild(error)
+        // console.log('vaild')
+    }
+    console.log(e);
+}
+function isEmail(email) { 
+    const re = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+    return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener('submit', checkEmail);
+
